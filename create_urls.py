160b82e3ab10eebb5633b7875084ba317b2663bf
Brandon Liu <bdon@bdon.org>
2021-09-04 09:27:24 -0700

    add argument parsing for maxzoom and bbox [#1]

--- create_urls.py
+++ create_urls.py
@@ -6,6 +6,26 @@ import sys
 import urllib.request
 import lzma
 import math
+import argparse
+
+parser = argparse.ArgumentParser(description='Create a urls.txt for siege.')
+parser.add_argument('--maxzoom', type=int, default=19, help='Maximum zoom level, inclusive.')
+parser.add_argument('--bbox', type=str,help='Bounding box: min_lon,min_lat,max_lon,max_lat')
+args = parser.parse_args()
+
+def _xy(lon,lat):
+    x = lon/360.0 + 0.5
+    sinlat = math.sin(math.radians(lat))
+    y = 0.5 - 0.25 * math.log((1.0 + sinlat) / (1.0 - sinlat)) / math.pi
+    return x,y
+
+bounds = None
+
+if args.bbox:
+    min_lon, min_lat, max_lon, max_lat = args.bbox.split(',')
+    min_x, min_y = _xy(float(min_lon),float(min_lat))
+    max_x, max_y = _xy(float(max_lon),float(max_lat))
+    bounds = [min_x,max_y,max_x,min_y] # invert Y
 
 # one week of anonymized tile edge request logs from openstreetmap.org
 FILENAME = 'tiles-2021-08-08.txt.xz'
@@ -19,6 +39,7 @@ if not os.path.isfile(FILENAME):
 random.seed(3857)
 
 maxzoom = 19
+distribution = [2,2,6,12,16,27,38,41,49,56,72,71,99,135,135,136,102,66,37,6] # the total distribution...
 
 counts = {}
 for zoom in range(maxzoom+1):
@@ -36,6 +57,15 @@ with lzma.open(FILENAME,'rt') as f:
         z = int(split[0])
         x = int(split[1])
         y = int(split[2])
+
+        if bounds:
+            f = 1 << z
+            if (x >= math.floor(bounds[0] * f) and
+               x <= math.floor(bounds[2] * f) and
+               y >= math.floor(bounds[1] * f) and
+               y <= math.floor(bounds[3] * f)):
+                pass
+
         count = int(row[1])
         counts[z] = counts[z] + count
 
@@ -61,5 +91,3 @@ with open('urls.txt','w') as f:
         i = bisect.bisect(ranges,rand)
         f.write(f"$(PROT)://$(HOST):$(PORT)/$(PATH){tiles[i]}.$(EXT)\n")
 print(f"wrote urls.txt with {OUTPUT_ROWS} requests.")
-
-
